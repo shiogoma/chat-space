d971569e2081778dafa6888fb90fc0139215f67f
shiogoma <azm0820@gmail.com>
2019-08-08 21:24:31 +0900

    create-api

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,7 +34,9 @@ $(document).on('turbolinks:load', function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
+      $('.form__submit').click("disabled");
+      $('#new_message')[0].reset();
+    });
       $('.form__submit').prop( "disabled", false );
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
