6d54cf3e800364924083870b7304eee0bc145f5e
shiogoma <azm0820@gmail.com>
2019-08-10 16:40:39 +0900

    auto-message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function(){
   function buildHTML(message){
     var image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                     ${message.user_name}
@@ -34,7 +34,6 @@ $(document).on('turbolinks:load', function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__submit').prop( "disabled", false );
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('.form__submit').click("disabled");
       $('#new_message')[0].reset();   
@@ -43,5 +42,34 @@ $(document).on('turbolinks:load', function(){
     .fail(function(){
       alert('error');
     })  
+    .always(function() {
+      $('.form__submit').prop('disabled',false);
+    })
   })
+  
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
+      
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(mesages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+          $(".messages").animate({ scrollTop: $(".chat__body")[0].scrollHeight }, "fast");
+        
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
