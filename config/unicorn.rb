d0f6bef39c30367e0e5e758c56416ad2bb4e0053
shiogoma <azm0820@gmail.com>
2019-08-13 18:48:37 +0900

    api

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,6 @@ listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-listen 3000
 timeout 60
 
 
